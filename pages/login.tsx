2507c93672e9d71cbe8b949e95e599a9ad8243cf
yamazaki_spread1 <yamazaki@spread1.co.jp>
2023-05-24 18:53:31 +0900

    chatgptに聞きながらスピナーできた

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const LoginPage: FC = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-
+  const [isLoading, setIsLoading] = useState(false);
   const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
   };
@@ -12,10 +12,16 @@ const LoginPage: FC = () => {
     setPassword(event.target.value);
   };
 
-  const handleSubmit = (event: FormEvent) => {
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+    setIsLoading(true);
+
     // ここで認証ロジックを実装します。
+    // 仮に5秒待つ
+    await new Promise(resolve => setTimeout(resolve, 5000));
     console.log(`Logging in with username: ${username} and password: ${password}`);
+
+    setIsLoading(false);
   };
 
   return (
@@ -38,6 +44,12 @@ const LoginPage: FC = () => {
 
           <div>
             <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              {isLoading ? (
+                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+              ) : null}
               Login
             </button>
           </div>
