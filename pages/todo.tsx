0d513f630b7d50e9e54166a8344543976be3b8b8
yamazaki_spread1 <yamazaki@spread1.co.jp>
2023-05-29 18:24:39 +0900

    TODOが入力されているかどうかで条件分岐

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState, ChangeEvent, FormEvent, MouseEvent } from 'react';
 import Todo from '@/components/Todo';
 const TodoList: FC = () => {
   const [todoText, setTodoText] = useState<string>('');
+  const [todoList, setTodoList] = useState<Array<string>>([]);
   const handleTodoTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTodoText(event.target.value);
   };
@@ -13,6 +14,14 @@ const TodoList: FC = () => {
     event.preventDefault();
     alert("Remove!");
   };
+  const handleAddTodo = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (todoText) {
+      alert("Add!");
+    } else {
+      alert("Todo is empty!");
+    }
+  }
   return (
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
       <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
@@ -20,7 +29,7 @@ const TodoList: FC = () => {
           <h1 className="text-grey-darkest">Todo List</h1>
           <div className="flex mt-4">
             <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 placeholder-gray-500 text-gray-900" placeholder="Add Todo" value={todoText} onChange={handleTodoTextChange}/>
-            <button className="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add</button>
+            <button onClick={handleAddTodo} className="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add</button>
           </div>
         </div>
         <div>
