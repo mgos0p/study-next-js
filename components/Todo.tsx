0ba79c44b2de277ead207dfdb01ff189cbff7a74
yamazaki_spread1 <yamazaki@spread1.co.jp>
2023-05-29 17:33:29 +0900

    tailwind触りながら位置とフォント調整

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Todo = () => (
   <div className="flex mb-4 items-center">
-    <p className="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-    <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
-    <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
+    <p className="text-left text-gray-900">Add another component to Tailwind Components</p>
+    <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white-500 text-green-500 border-green-500 hover:bg-green-500">Done</button>
+    <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Remove</button>
   </div>
 );
 
--- pages/todo.tsx
+++ pages/todo.tsx
@@ -8,7 +8,7 @@ const TodoList: FC = () => {
           <h1 className="text-grey-darkest">Todo List</h1>
           <div className="flex mt-4">
             <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo"/>
-            <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add</button>
+            <button className="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add</button>
           </div>
         </div>
         <div>
