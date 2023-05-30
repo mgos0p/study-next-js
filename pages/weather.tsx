f95debc638f4893f1434594135ca080149fa1cef
mgos0p <bump114jp@gmail.com>
2023-05-30 15:26:05 +0900

    ChatGPTに聞きながらAPI呼び出し成功。 多分天気予報だとリアルタイム性が重視されそうなのでクライアントサイドでレンダリングした方が良さそう。

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 type Weather = {
   publicTime:string;
   publicTimeFormatted:string;
@@ -59,8 +59,56 @@ type Copyright = {
   image:string;
   provider:string;
 }
+// サーバーサイドレンダリングSSR
+// サーバー側で実行され、リクエスト時にデータをフェッチ
+// export async function getServerSideProps(context) {
+//   const res = await fetch('https://weather.tsukumijima.net/api/forecast?city=130010')
+//   const data = await res.json()
+
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   }
+// }
+
+// 静的生成SSG
+// サーバー側で実行され、ビルド時にデータをフェッチ
+// export async function getStaticProps() {
+//   const res = await fetch('https://weather.tsukumijima.net/api/forecast?city=130010')
+//   const data = await res.json()
+
+//   return {
+//     props: {
+//       data,
+//     },
+//     revalidate: 1, // Optional: Re-generate the page every 1 second
+//   }
+// }
 
 const WeatherPage: FC = () => {
   const [weather, setWeather] = useState<Weather>();
+  // クライアントサイドレンダリングCSR
+  // ブラウザ側で実行され、実行するたびにデータをフェッチする。
+  // ユーザー固有データ、リアルタイム性を求める場合に適している。
+  useEffect(() => {
+    async function fetchData() {
+      const endpoint = `https://weather.tsukumijima.net/api/forecast?city=130010`;
+
+      if (!endpoint) {
+        throw new Error('WEATHER_API_ENDPOINT is not defined');
+      }
+
+      const res = await fetch(endpoint);
+      const newData = await res.json();
+      setWeather(newData);
+    }
+
+    fetchData();
+  }, []);
 };
 export default WeatherPage;
